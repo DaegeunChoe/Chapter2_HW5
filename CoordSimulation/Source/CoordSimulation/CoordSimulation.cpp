6ba8ba18789cee7483fea60a4d25cdf95d127efa
DaegeunChoe <asdf12gg@gmail.com>
2025-06-16 10:13:06 +0900

    Generate UE project

---
+++ CoordSimulation/Source/CoordSimulation/CoordSimulation.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CoordSimulation.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoordSimulation, "CoordSimulation" );
